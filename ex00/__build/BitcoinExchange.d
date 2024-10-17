25977405b06981caf58c4c45531ce2376783f665
OUSSAMA BENCHKRON SAID <obenchkroune01@gmail.com>
2024-10-17 01:17:59 +0100

    remove build files from vcs

---
+++ ex00/__build/BitcoinExchange.d
@@ -0,0 +1,3 @@
+__build/BitcoinExchange.o: srcs/BitcoinExchange.cpp \
+ include/BitcoinExchange.hpp
+include/BitcoinExchange.hpp:
---
+++ ex00/__build/main.d
@@ -0,0 +1,2 @@
+__build/main.o: srcs/main.cpp include/BitcoinExchange.hpp
+include/BitcoinExchange.hpp:
